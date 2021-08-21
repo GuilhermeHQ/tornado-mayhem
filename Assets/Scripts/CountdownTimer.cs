732b84e8120c3fed47f45e3871cccb45e641112b
guilherme_hq <quintana.guilherme@gmail.com>
2021-08-21 12:10:01 -0300

    add countdowntimer/ update project settings

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    [SerializeField] float startingTime = 10f;
+    [SerializeField] Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 10)
+        {
+            countdownText.color = Color.red;
+        }        
+
+        if (currentTime <=0)
+        {
+            currentTime = 0;
+        }
+
+    }
+
+}
